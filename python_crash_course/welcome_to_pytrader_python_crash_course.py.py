59845db015295dbf8e61153c344c37ecaeaca931
miguelcoria94 <miguelcoria94@gmail.com>
2021-03-30 23:49:47 -0500

    made changes

---
+++ python_crash_course/welcome_to_pytrader_python_crash_course.py.py
@@ -0,0 +1,21 @@
+#####################################
+#### python crash course - overview ####
+#####################################
+
+"""
+
+Welcome to the python crash course
+
+In this series of videos we are going to be learning python.
+
+When learning python or any other coding language it can feel overwhelming.
+There's so much to learn and you will at times feel like there's no way you be able to memorize everything.
+Please don't do that. If you try to learn everything you will burn yourself out and give up learning completely.
+Throughout this course I want you to focus on what's in front of you. I designed this course so that
+we can focus ONLY on the things that will help us reach our goal.
+Which is learning python for technical analysis and algorithmic trading.
+
+in the next video we will talk about what is python and go over 
+
+"""
+
